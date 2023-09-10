77dd2a6183bd08108f33457a80ce264ee8bbe25b
Sijan Malla <sijan.malla101@gmail.com>
2023-09-10 10:19:55 -0500

    Added toggle <p> and button functionality

--- src/SingleQuestion.jsx
+++ src/SingleQuestion.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { AiOutlineMinus } from 'react-icons/ai';
-// AiOutlinePlus
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const SingleQuestion = ({ id, title, info }) => {
+  const [show, setShow] = useState(false);
+
   return (
     <article className="question">
       <header>
         <h5>{title}</h5>
-        <button className="question-btn">
-          <AiOutlineMinus />
+        <button className="question-btn" onClick={() => setShow(!show)}>
+          {show ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      <p>{info}</p>
+      {show && <p>{info}</p>}
     </article>
   );
 };
