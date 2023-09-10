8e955f57ef222d5948d1a93d5fdb91dbaedddcce
Sijan Malla <sijan.malla101@gmail.com>
2023-09-10 10:02:55 -0500

    Imported data and generated test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useState } from 'react';
+import questions from './data';
 const App = () => {
-  return <h2>Accordion Starter</h2>;
+  const [data, setData] = useState(questions);
+  return (
+    <main>
+      <section className="container">
+        <h1>Questions</h1>
+        <ul>
+          {data.map((d) => {
+            return <li key={d.id}>{d.title}</li>;
+          })}
+        </ul>
+      </section>
+    </main>
+  );
 };
 export default App;
